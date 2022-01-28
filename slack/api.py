4ea2b8e90d40d478c0d00ea699d346babfa6323b
Ulrich Hinze <ulrich@hinze.fi>
2022-01-28 11:46:15 +0100

    Add initial message (#4)
    * Add initial message
    
    * Fix imports
    
    Co-authored-by: Ulrich Hinze <ulrich.hinze@bkk-vbu.de>

--- slack/api.py
+++ slack/api.py
@@ -2,24 +2,29 @@ import json
 import os
 import time
 
-# import requests
 import boto3 as boto3
+from botocore.exceptions import ClientError
+
+from notification import notify, newuser_notification
 
 client = boto3.client('dynamodb')
 
 
 def update_item(id: str, ttl: str, insert_if_not_exists: bool):
-    return client.update_item(
-        TableName=os.environ["TABLE_NAME"],
-        Key={"id": {
-            "S": id
-        }},
-        UpdateExpression="SET timetolive = :ttl",
-        ExpressionAttributeValues={
+    args = {
+        'TableName': os.environ["TABLE_NAME"],
+        'Key': {"id": {"S": id
+                       }},
+        'UpdateExpression': "SET timetolive = :ttl",
+        'ExpressionAttributeValues': {
             ":ttl": {
                 "N": ttl
             }
-        })
+        }
+    }
+    if not insert_if_not_exists:
+        args['ConditionExpression'] = "attribute_exists(id)"
+    return client.update_item(**args)
 
 
 def lambda_handler(event, context):
@@ -31,8 +36,13 @@ def lambda_handler(event, context):
     ttl = str(int(time.time() + ttl_minutes * 60))
     print(f"Calculated timestamp: {ttl}")
 
-    response = update_item(slack_path, ttl, False)
-
+    try:
+        response = update_item(slack_path, ttl, False)
+    except ClientError as err:
+        print(err.operation_name)
+        if err.response['Error']['Code'] == 'ConditionalCheckFailedException':
+            response = update_item(slack_path, ttl, True)
+            notify(slack_path, newuser_notification)
     print(f"Updated item in DynamoDB: {json.dumps(response)}")
 
     return {
